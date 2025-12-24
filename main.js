5917474ba9e2e1a04d30074f978166709b17d1d5
Hussain <hussain.dev94@gmail.com>
2025-12-24 12:13:42 +0300

    coding transition between active and normal state.

--- main.js
+++ main.js
@@ -4,11 +4,13 @@
 const shareButton = document.querySelector(".share-button");
 const circle = document.querySelector("circle");
 const arrow = document.querySelector("path");
+const socialButtons = document.querySelector(".social-buttons");
 
 function toggleShareButton(e) {
     e.preventDefault();
-    circle.classList.toggle("dark-background");
-    arrow.classList.toggle("light-background");
+    circle.classList.toggle("dark-fill");
+    arrow.classList.toggle("light-fill");
+    socialButtons.classList.toggle("hidden");
 }
 
 //2. Add event listner to the shareButton;
