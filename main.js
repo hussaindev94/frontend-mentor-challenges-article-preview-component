bee51f6e35a4a0392c902e61d181cab33e18d77e
Hussain <hussain.dev94@gmail.com>
2025-12-25 03:14:05 +0300

    Finish tablet layout style in active state.

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-
-
-//1. Grap the share button
+//1. Grap the elements:
 const shareButton = document.querySelector(".share-button");
 const circle = document.querySelector("circle");
 const arrow = document.querySelector("path");
 const socialButtons = document.querySelector(".social-buttons");
+const messageLikeShape = document.querySelector(".message-like-shape");
+
+
 
 function toggleShareButton(e) {
     e.preventDefault();
     circle.classList.toggle("dark-fill");
     arrow.classList.toggle("light-fill");
     socialButtons.classList.toggle("hidden");
+
+    if (window.innerWidth > 704) {
+        messageLikeShape.classList.toggle("block-display");
+    }
 }
 
 //2. Add event listner to the shareButton;
